58f41120ee800094f965b45f1d444cc60dabeaad
harapeko <tea.lile.tea@gmail.com>
2017-12-22 00:04:02 +0900

    vuexの大まかな枠組みを追加した

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,24 +3,29 @@
     HeadComp
     TextareaComp
     StringComp
+    button(@click="buttonAction") {{ button }}
 </template>
 
 <script>
 import HeadComp from '@/components/modules/HeadComp'
 import TextareaComp from '@/components/modules/TextareaComp'
 import StringComp from '@/components/modules/StringComp'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'FormComp',
   data () {
     return {
-      msg: 'ようこそでち'
+      button: '確認'
     }
   },
+  methods: mapActions('Form', {
+    'buttonAction': 'buttonAction'
+  }),
   components: {
     HeadComp,
     TextareaComp,
     StringComp,
-  }
+  },
 }
 </script>
