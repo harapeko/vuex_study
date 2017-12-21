362562908b4bf7db245424a1b874e60b50389e56
harapeko <tea.lile.tea@gmail.com>
2017-12-21 10:56:59 +0900

    モジュール作成。Form.vueをコピペした

---
+++ src/components/modules/HeadComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This is Form Page</div>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
