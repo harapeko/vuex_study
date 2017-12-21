2aa7d066194da0c8c3307ae000b9b179785b5371
harapeko <tea.lile.tea@gmail.com>
2017-12-21 10:45:31 +0900

    Form.vueコンポーネントを作成した

---
+++ src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>this is Form Page</div>
+</template>
+
+<script>
+export default {
+  name: 'from',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
