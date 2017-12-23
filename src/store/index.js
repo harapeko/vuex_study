354fdf5e8aa7714c0a94baf481c1e777a5f6dd7b
harapeko <tea.lile.tea@gmail.com>
2017-12-23 20:44:14 +0900

    入力必須バリデーション対応した

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ Vue.use(Vuex)
 const Form = {
   namespaced: true,
   state: {
-    button: ["確認", "送信"]
+    button: ["確認", "送信", "戻る"]
   },
   mutations: {},
   actions: {
@@ -36,10 +36,28 @@ const Head = {
   },
 }
 
+const Textarea = {
+  namespaced: true,
+  state: {
+    errorMsg: "入力は必須です",
+  },
+  getters: {
+    getError(state, getters, rootState) {
+      if(rootState.errorFlag) {
+        return null
+      } else {
+        return state.errorMsg
+      }
+    }
+  }
+}
+
 export default new Vuex.Store({
   state: {
     stepMax: 3,
     stepCount: 0,
+    impression: "",
+    errorFlag: false, //trueなら通貨
   },
   mutations: {
     setStepCount (state) {
@@ -50,10 +68,20 @@ export default new Vuex.Store({
       if(state.stepCount == state.stepMax) {
         state.stepCount = 0
       }
-    }
+    },
+    updateImpression(state, value) {
+      state.impression = value
+  
+      if (state.impression) {
+        state.errorFlag = true
+      } else {
+        state.errorFlag = false
+      }
+    },
   },
   modules: {
     Form,
-    Head
+    Head,
+    Textarea
   }
 })
